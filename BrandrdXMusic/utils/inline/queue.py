02220c5188314bab5d843c55f2210d6c3cbbedae
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-04-30 12:22:37 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/II_ROYALENTRY1128_II"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/BRANDEDKING8"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/ll_DPZ_WORLDS_ll"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
