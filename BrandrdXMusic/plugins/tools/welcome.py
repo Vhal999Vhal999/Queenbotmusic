4d4cbfc436ec97e9944bba35d19e25ea151f1f02
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-05-01 09:23:36 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -91,10 +91,10 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}
 ๏ 𝗜𝗗 ➠ {user.id}
 ๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ @{user.username}
-๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ @SONYMIXMUSICBOT ❤
+๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ @ROYALXMUSICBOT ❤
 ➖➖➖➖➖➖➖➖➖➖➖
 """,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/BRANDED_KUDI_BOT?startgroup=true")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/ROYALXMUSICBOT?startgroup=true")]])
         )
     except Exception as e:
         LOGGER.error(e)
